a3da77ec64cc402585b75fb20bd734fbb6e8443c
Eric Xu <ericxu2048@gmail.com>
2022-12-31 09:17:45 -0500

    Add self-hosted font

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 import { BrowserRouter } from "react-router-dom";
+import theme from "./theme"
+import "@fontsource/roboto/400.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ColorModeScript />
     <BrowserRouter basename="/crystalcube">
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,12 @@ import { extendTheme } from "@chakra-ui/react"
 
 const config = {
   initialColorMode: "dark",
+  fonts: {
+    heading: `'Roboto', sans-serif`,
+    body: `'Roboto', sans-serif`,
+  }
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme(config)
 
 export default theme
