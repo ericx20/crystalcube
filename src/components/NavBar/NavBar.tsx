4f6b72ed9d8775928fce0831582f15881078a408
Eric Xu <ericxu2048@gmail.com>
2023-01-04 18:04:31 -0500

    Fix navbar position

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -33,17 +33,19 @@ export default function NavBar() {
   const { isOpen, onToggle, onClose } = useDisclosure()
 
   return (
-    <Box>
+    <Box position="fixed" w="100%" zIndex={3}>
       <Flex
-        bg={useColorModeValue("white", "gray.800")}
+        bg={useColorModeValue("#FFFFFF99", "#1A202C99")}
         color={useColorModeValue("gray.600", "white")}
-        minH="60px"
+        backdropFilter="blur(5px)"
+        h={14}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
         borderStyle="solid"
         borderColor={useColorModeValue("gray.200", "gray.900")}
-        align="center">
+        align="center"
+      >
         <Flex
           flex={{ base: 1, md: "auto" }}
           ml={-2}
