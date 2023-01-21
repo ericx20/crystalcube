079e71547839665ac46ea05ac2a016d75641dd05
Eric Xu <ericxu2048@gmail.com>
2023-01-21 16:32:37 -0500

    Add n-move scrambles to library

--- src/lib/search/scramble.ts
+++ src/lib/search/scramble.ts
@@ -35,6 +35,43 @@ function nFlipEOArray(n: number): Array<number> {
   return shuffle(goodEdges.concat(badEdges))
 }
 
+// generate scrambles that need n moves to solve optimally
+// n must be integer from 0 to solverConfig.nMoveScrambleLimit
+// TODO: add nMoveScrambleLimit and nMoveScrambleMaxIterations to solver configs
+// TODO: make this async
+export function nMoveScrambleForSolver(n: number, solverName: SolverConfigName, preRotation: Array<CubeRotation> = []): MoveSeq | null {
+  const MAX_ITERATIONS = 200
+  const solverConfig = SOLVER_CONFIGS[solverName]
+
+  // TODO: change this to solverConfig.nMoveScrambleLimit
+  if (n > solverConfig.depthLimit) {
+    return null
+  }
+
+  // the optimal solution for an n-move sequence will always be at most n
+  // e.g. if you scramble with 3 moves, the optimal solution will always be at most 3 moves long
+  // therefore optimalSolutionLength <= n
+  let scramble: MoveSeq = randomMoves(n)
+  for (let i = 0; i < MAX_ITERATIONS; i++) {
+    console.log(`iteration #${i+1}`);
+    // console.log(scramble.join(" "))
+    // check pruning table to see if it has length of optimal solution
+    const solvedCube = applyMask(SOLVED_FACELET_CUBE, solverConfig.mask)
+    const scrambledCube = applyMoves(solvedCube, scramble)
+    const stringCube = faceletCubeToString(scrambledCube)
+    const optimalSolutionLength = getPruningTable(solverName)[stringCube]
+      ?? solve(scramble, solverName, preRotation, 1)[0].length
+    if (optimalSolutionLength === n) {
+      // the scramble is the right difficulty, bingo
+      return simplifyScramble(scramble, solverName, preRotation)
+    }
+
+    // the scramble is not hard enough, add an extra move
+    appendRandomMove(scramble)
+  }
+  return null
+}
+
 export function simplifyScramble(
   moves: MoveSeq,
   solverName: SolverConfigName,
--- src/pages/SecretTestPage.tsx
+++ src/pages/SecretTestPage.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import { nMoveScrambleForSolver } from "src/lib"
 import { NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from "@chakra-ui/react"
 
 export default function SecretTestPage() {
   const [difficulty, setDifficulty] = useState(0)
+  console.log(nMoveScrambleForSolver(difficulty, "EOCross")?.join(" "))
   return (
     <NumberInput value={difficulty} onChange={(_, val) => setDifficulty(val)} min={0} max={10}>
     <NumberInputField />
