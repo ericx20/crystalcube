66982f9fff668923a27f663305b5072050c1724f
Eric Xu <ericxu2048@gmail.com>
2023-09-06 13:33:00 -0400

    Change error submission to Google Forms

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -3,9 +3,10 @@ import {
   AlertIcon,
   AlertTitle,
   AlertDescription,
-  Box,
   Link,
   Text,
+  Button,
+  VStack,
 } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 
@@ -16,35 +17,32 @@ interface FallbackProps {
 export default function Fallback({ error }: FallbackProps) {
   const errorMessage =
     error instanceof Error ? error.message : "an unknown error occurred :(";
-  const stackTrace = error instanceof Error ? error.stack : "none";
-  const url = new URL("https://github.com/ericx20/crystalcube/issues/new");
-  url.searchParams.append("title", `Error: ${errorMessage}`);
-  url.searchParams.append(
-    "body",
-    `Describe the problem:
-Operating system and browser:
+  const stackTrace = error instanceof Error ? error.stack ?? "" : "none";
 
-Stack trace:
-\`\`\`
-${stackTrace}
-\`\`\`
-`
-  );
-  url.searchParams.append("labels", "bug");
+  const errorDetails = `error message: ${errorMessage}
+
+stack trace: ${stackTrace}`;
+  const onCopy = () => navigator.clipboard.writeText(errorDetails);
+
   return (
     <Alert status="error">
       <AlertIcon />
-      <Box>
-        <AlertTitle>something went wrong:</AlertTitle>
-        <AlertDescription>{errorMessage}</AlertDescription>
-        <Text>
-          please open an issue on{" "}
-          <Link href={url.toString()} isExternal color="teal.500">
-            GitHub
+      <VStack align="flex-start">
+        <AlertTitle>something went wrong!</AlertTitle>
+        <AlertDescription>
+          please fill this{" "}
+          <Link
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdjq3LygpYhUmzPpwStSizGqOcPH3JYeQToezyp4qK9FtZl3w/viewform"
+            isExternal
+            color="teal.500"
+          >
+            form
             <ExternalLinkIcon mx="2px" />
           </Link>
-        </Text>
-      </Box>
+          so we can fix it
+        </AlertDescription>
+        <Button onClick={onCopy}>copy error details</Button>
+      </VStack>
     </Alert>
   );
 }
